8c3edf86885990123eba3f930b3887f8188b30dd
amirzad <amirzad.personal@gmail.com>
2026-07-27 13:12:43 +0330

    fix cors

--- src/main/java/ir/manaz/config/CorsConfig.java
+++ src/main/java/ir/manaz/config/CorsConfig.java
@@ -10,12 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://manaz.pro",
-                        "https://www.manaz.pro",
-                        "http://localhost:3000",
-                        "http://localhost:5173"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
