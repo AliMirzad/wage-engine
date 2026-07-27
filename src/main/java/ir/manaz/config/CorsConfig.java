f401b3605a719b8c166bb5cf9df3919ff0006680
amirzad <amirzad.personal@gmail.com>
2026-07-27 14:43:51 +0330

    Revert "fix cors"
    This reverts commit 8c3edf86885990123eba3f930b3887f8188b30dd.

--- src/main/java/ir/manaz/config/CorsConfig.java
+++ src/main/java/ir/manaz/config/CorsConfig.java
@@ -10,7 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins(
+                        "https://manaz.pro",
+                        "https://www.manaz.pro",
+                        "http://localhost:3000",
+                        "http://localhost:5173"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
